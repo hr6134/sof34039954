7649b6e8734190cfb7fd59c55e9dd1d66edde35d
ltoshchev <ltoshchev@aplana.com>
2015-12-02 13:58:37 +0300

    - add static fabric method - make Book class immutable - add try with resource for Scanner - remove unnecessary getters - remove unnecessary constructor from BookException

--- main/src/Book.java
+++ main/src/Book.java
@@ -1,71 +1,55 @@
 /**
+ * Immutable book class.
+ * Instance could be created through static factory method.
+ *
  * @author hr6134
  *         02.12.15
  */
 public class Book {
-    //instance variables
-    private String title = "";
-    private int isbn;
-    private int quantity;
+    private final String title;
+    private final int isbn;
+    private final int quantity;
 
-    public Book(String title, int isbn, int quantity) throws BookException {
-        //constructors
-        settitle(title);
-        setquantity(quantity);
-        setisbn(isbn);
+    private Book(String title, int isbn, int quantity) {
+        this.title = title;
+        this.isbn = isbn;
+        this.quantity = quantity;
     }
 
-    public String toString() { //toString Method
-
-        String s = "";
-        s = s + "Title: " + this.title + "\nISBN: " + this.isbn
-                + "\nQuantity: " + this.quantity + "\n";
-        return s;
-
+    public static Book create(String title, int isbn, int quantity) throws BookException {
+        validateTitle(title);
+        validateIsbn(isbn);
+        validateQuantity(quantity);
+        return new Book(title, isbn, quantity);
     }
 
-    public String gettitle() {
-        return this.title;
-    }
-
-    public int getisbn() {
-        return this.isbn;
-    }
-
-    public int getquantity() {
-        return this.quantity;
-    }
-
-    //mutator methods
-    public void settitle(String newtitle) throws BookException {
-        if (newtitle.length() < 1) {
+    private static void validateTitle(String title) throws BookException {
+        if (title.length() < 1) {
             BookException be = new BookException();
             be.setMessage("Title cannot be blank");
             throw be;
-        } else {
-            this.title = newtitle;
         }
     }
 
-    public void setisbn(int newisbn) throws BookException {
-        if (newisbn >= 1000 && newisbn >= 10000) {
-            this.isbn = newisbn;
-        } else {
+    private static void validateIsbn(int isbn) throws BookException {
+        if (isbn <= 1000 || isbn >= 10000) {
             BookException be = new BookException();
             be.setMessage("ISBN should be between 1000 and 10000.");
             throw be;
         }
     }
 
-    public void setquantity(int newquantity) throws BookException {
-        if (newquantity >= 0) {
-            this.quantity = newquantity;
-        } else {
+    private static void validateQuantity(int quantity) throws BookException {
+        if (quantity < 0) {
             BookException be = new BookException();
             be.setMessage("Quantity can't be a negative number.");
             throw be;
         }
     }
 
+    @Override
+    public String toString() {
+        return String.format("Title: %s \nISBN: %s \nQuantity: %s", title, isbn, quantity);
+    }
 }
 
--- main/src/BookException.java
+++ main/src/BookException.java
@@ -6,11 +6,6 @@ public class BookException extends Exception {
     //instance variable
     private String message = "";
 
-    public BookException() {
-//empty constructor
-
-    }
-
     public void setMessage(String newMessage) {
         this.message = newMessage;
     }
